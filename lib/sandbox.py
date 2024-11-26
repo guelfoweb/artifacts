178c730c661d2871acc3c1ae194db60bddf2b61e
Andrea Draghetti <drego85@draghetti.it>
2024-11-26 10:33:22 +0100

    Integrated MobSD SandBox

--- lib/sandbox.py
+++ lib/sandbox.py
@@ -1,10 +1,13 @@
 def url(md5):
-	sandboxes_url = [
-		"https://tria.ge/s?q=",
-		"https://www.joesandbox.com/analysis/search?q=",
-		"https://www.virustotal.com/gui/search/",
-		"https://bazaar.abuse.ch/browse.php?search=md5:",
-		"https://koodous.com/apks?search="
-		]
-	sandboxes = [sandbox+md5 for sandbox in sandboxes_url]
-	return sandboxes
+    sandboxes_url = [
+        "https://tria.ge/s?q={md5}",
+        "https://www.joesandbox.com/analysis/search?q={md5}",
+        "https://www.virustotal.com/gui/search/{md5}",
+        "https://bazaar.abuse.ch/browse.php?search=md5:{md5}",
+        "https://koodous.com/apks?search={md5}",
+        "https://mobsf.live/static_analyzer/{md5}/"
+    ]
+	
+    sandboxes = [sandbox.format(md5=md5) for sandbox in sandboxes_url]
+    
+    return sandboxes
